bd992413be6b1c05292126c487118f1b0ca8bff3
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-02-16 09:17:13 -0500

    Cleaned up app scripts

--- app/User.js
+++ app/User.js
@@ -2,16 +2,17 @@
  * 获取当前用户信息
  */
 var http = require('http');
-var currentUser = function()
-{
-    
+var currentUser = function () {
 }
+
 /**
  * 用户登录
  */
-var Login = function(){};
+var Login = function(){
+};
 
 /**
  * 注销用户
  */
-var logout = function(){};
+var logout = function(){
+};
--- app/window.js
+++ app/window.js
@@ -1,12 +1,14 @@
 ﻿/**
  * Get current window
  */
-var currentWin = function(){
-  try {
-    var win = nw.Window.get();
-    alert(win.title);
-  } catch (error) {
-      alert(error);
-  }
+
+var currentWin = function () {
+    try {
+        var win = nw.Window.get();
+        alert(win.title);
+    } catch (error) {
+        alert(error);
+    }
 };
+
 exports.currentWin = currentWin;
